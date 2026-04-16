74a5d48502d450049ba7f72b38aaa7a84e20b6cb
Daria Drozhdina <dariadrozhdina@Darias-MacBook-Air-2.local>
2026-04-16 15:37:54 +0200

    added prep

---
+++ models/prep/prep_weather_daily.sql
@@ -0,0 +1,27 @@
+WITH daily_data AS (
+    SELECT * 
+    FROM staging_weather_daily
+),
+add_features AS (
+    SELECT *
+		, date_part('day', date) AS date_day 		-- number of the day of month
+		, date_part('month', date) AS date_month 	-- number of the month of year
+		, date_part('year', date) AS date_year 		-- number of year
+		, DATE_PART('week', date)AS cw 			-- number of the week of year
+		, TO_CHAR(date, 'FMmon') AS month_name 	-- name of the month
+		, TO_CHAR(date, 'FMdy') AS weekday 		-- name of the weekday
+    FROM daily_data 
+),
+add_more_features AS (
+    SELECT *
+		, (CASE 
+			WHEN month_name in ('Dec','Jan','Feb') THEN 'winter'
+			WHEN month_name in ('Mar','Apr','May') THEN 'spring'
+            WHEN month_name in ('Jun','Jul','Aug') THEN 'summer'
+            WHEN month_name in ('Sep','Oct','Nov') THEN 'autumn'
+		END) AS season
+    FROM add_features
+)
+SELECT *
+FROM add_more_features
+ORDER BY date
